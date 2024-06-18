25f04a35d7390f9e8ce6701445a5f6d896c9ac8d
oputaolivia <oputaolivia123@gmail.com>
2024-06-18 22:35:10 +0100

    Azure Function works

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const { CosmosClient } = require("@azure/cosmos");
+// const { CosmosClient } = require("@azure/cosmos");
+const express = require("express");
+const dotenv = require("dotenv");
+const axios = require("axios")
 
-const client = new CosmosClient(process.env.COSMOS_CONNECTION_STRING);
-const database = client.database("nexahub");
-const container = database.container("hub");
+
+const app = express();
+dotenv.config();
+const PORT = 8080
+
+app.use(express.json());
+// const client = new CosmosClient(process.env.COSMOS_CONNECTION_STRING);
+// const database = client.database("nexahub");
+// const container = database.container("hub");
 
 app.post("/api/submit", async (req, res) => {
-  const { name, email, type, message } = req.body;
-
-  // Validate input
+  let { name, email, type, message } = req.body;
   if (!name || !email || !type || !message) {
     return res.status(400).json({ error: "All fields are required" });
   }
-
-  // Store the complaint in the database
   const complaint = { name, email, type, message, date: new Date() };
   try {
-    await container.items.create(complaint);
-
-    // Trigger email notification
-    await axios.post(process.env.AZURE_FUNCTION, complaint);
+    // await container.items.create(complaint);
+    const sent = await axios.post(process.env.AZURE_FUNCTION, complaint);
+    // console.log(sent)
     res.status(200).json({ message: "Complaint submitted successfully" });
   } catch (error) {
     res.status(500).json({ error: "Failed to submit complaint" });
   }
+});
+
+app.listen(PORT, () => {
+  console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
